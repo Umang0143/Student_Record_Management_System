a62b943ac522146319f118023e382c04adeda46f
Umang0143 <umangrajput1995@gmail.com>
2025-04-15 00:28:37 +0530

    name correction

--- Umang/Search_Student.py
+++ Umang/Search_Student.py
@@ -11,45 +11,47 @@ def searchstudent(path):
     with open(path, "r") as file:
         students = json.load(file)
     while True:
-        print("----- Search by -----")
-        print("press 1. Id")
-        print("press 2. Name")
-        print("press 3. Contact")
-        print("press 4. Address")
-        print("press 5. Email Id")
-        print("press 0. Exit Searching Area")
-        print("=" * 30)
+        print("=" * 40)
+        print("       Search By Options")
+        print("=" * 40)
+        print("1. Search by ID")
+        print("2. Search by Name")
+        print("3. Search by Contact Number")
+        print("4. Search by Address")
+        print("5. Search by Email ID")
+        print("0. Exit Searching Area")
+        print("=" * 40)
 
         try:
-            data = int(input("Please select an option (0-5): "))
-            if data in [0, 1, 2, 3, 4, 5]:
+            choice = int(input("Please select an option (0-5): "))
+            if choice in [0, 1, 2, 3, 4, 5]:
                 break
             else:
                 print("\nInvalid option! Please select a valid number (0-5).\n")
         except Exception as c :
             print("Invalid input! Please enter a digits")
-            data={"error":str(c) ,"date":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
-            Writelogs.writelogs(str(data))
+            details={"error":str(c) ,"date":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+            Writelogs.writelogs(str(details))
     
-    if data == 0:
+    if choice == 0:
         print("\nExiting for Searching!")
         return True
-    if data == 1:
+    if choice == 1:
         user="id"
         uservalue = str(Get_valid.get_valid_number("Enter your ID:- "))
-    if data == 2:
+    if choice == 2:
         user="name"
         uservalue = Get_valid.get_valid_name("Enter your Name:- ")
-    if data == 3:
+    if choice == 3:
         user="contact"
         uservalue = Get_valid.get_valid_contact("Enter your Contect:- ")
-    if data == 4:
+    if choice == 4:
         user="address"
         uservalue = input("Enter Your Address:- ")
-    if data == 5:
+    if choice == 5:
         user="email Id"
         uservalue = input("Enter your Email Id:- ")
-    
+
     found=False
 
     for listdata in students:
