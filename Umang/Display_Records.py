4f9a8284bbd9dd699f17cf51d334b7282699bf84
Umang0143 <umangrajput1995@gmail.com>
2025-04-15 14:52:52 +0530

    correction for file is not found

--- Umang/Display_Records.py
+++ Umang/Display_Records.py
@@ -1,6 +1,8 @@
 import os
 
 def displayrecords(path):
-    if os.path.exists(path):
-        with open(path, "r") as file:
-            print(file.read())
+    if not os.path.exists(path):
+        print("File is not found.")
+        return
+    with open(path, "r") as file:
+        print(file.read())
